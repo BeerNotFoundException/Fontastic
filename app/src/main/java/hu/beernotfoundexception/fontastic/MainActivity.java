6696528900e3dfae5fef44c8b33739a505037ee3
Krisztiaan <krisz011@gmail.com>
2016-02-12 15:31:03 +0100

    some utterly useless ui prettyfication, and an ip friendly textbox

--- app/src/main/java/hu/beernotfoundexception/fontastic/MainActivity.java
+++ app/src/main/java/hu/beernotfoundexception/fontastic/MainActivity.java
@@ -6,8 +6,9 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.EditText;
+
+import hu.beernotfoundexception.fontastic.util.StringUtil;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,31 +23,11 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "A camera should open here.", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        ((EditText) findViewById(R.id.txtIp)).addTextChangedListener(StringUtil.getIpTextWatcher());
     }
 }
